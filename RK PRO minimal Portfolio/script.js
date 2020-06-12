3e6a90f90e0404961f74c889fc53767f17aeb451
rkarumba <66837373+rkarumba@users.noreply.github.com>
2020-06-12 18:04:49 +0300

    Add files via upload

---
+++ RK PRO minimal Portfolio/script.js
@@ -0,0 +1,14 @@
+
+ScrollReveal().reveal('.team', { delay: 500, });
+ScrollReveal().reveal('.about', { delay: 1000 });
+
+
+
+function openNav() {
+    document.getElementById("mobile__menu").style.width = "100%"
+}
+
+function closeNav() {
+    document.getElementById("mobile__menu").style.width = "0"
+}
+
